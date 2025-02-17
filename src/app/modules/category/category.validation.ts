1e92f80603f317b68cb96477b22c4e16756129fa
mhlehri <lehrimirza101@gmail.com>
2025-02-18 05:12:48 +0600

    modify category.validation.ts (added sub category, isPublished field)

--- src/app/modules/category/category.validation.ts
+++ src/app/modules/category/category.validation.ts
@@ -4,6 +4,19 @@ export const categoryValidationSchema = z.object({
   name: z.string({
     message: "Name is required",
   }),
+  subCategories: z
+    .array(
+      z.object({
+        name: z.string({
+          message: "Name is required",
+        }),
+        image: z.string(),
+        isPublished: z.boolean().default(false),
+        isDeleted: z.boolean().default(false),
+      })
+    )
+    .optional(),
+  isPublished: z.boolean().default(false),
   image: z.string(),
   isDeleted: z.boolean().default(false).optional(),
 });
