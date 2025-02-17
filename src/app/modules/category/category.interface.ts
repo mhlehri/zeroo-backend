a30c01cd70609fb51f6c26b6f439a2e1f4830b49
mhlehri <lehrimirza101@gmail.com>
2025-02-18 05:08:42 +0600

    modify category.interface.ts (added sub category and type)

--- src/app/modules/category/category.interface.ts
+++ src/app/modules/category/category.interface.ts
@@ -1,5 +1,13 @@
 export type TCategory = {
   name: string;
+  subCategories?: TSubCategory[];
+  isPublished?: boolean;
+  image: string;
+  isDeleted: boolean;
+};
+export type TSubCategory = {
+  name: string;
+  isPublished?: boolean;
   image: string;
   isDeleted: boolean;
 };
