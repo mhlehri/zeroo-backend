66fbdd48595c1cc605b17cfa9a214d93428970a9
mhlehri <lehrimirza101@gmail.com>
2025-02-25 22:28:22 +0600

    added sub category validation

--- src/app/modules/category/category.validation.ts
+++ src/app/modules/category/category.validation.ts
@@ -20,3 +20,24 @@ export const categoryValidationSchema = z.object({
   }),
   isDeleted: z.boolean().default(false).optional(),
 });
+
+export const subCategoryValidationSchema = z.object({
+  name: z.string({
+    message: "Name is required",
+  }),
+  subCategories: z
+    .array(
+      z.object({
+        name: z.string({
+          message: "Name is required",
+        }),
+        isPublished: z.boolean().default(true).optional(),
+        isDeleted: z.boolean().default(false).optional(),
+      })
+    )
+    .optional(),
+  image: z.string({
+    message: "Image is required",
+  }),
+  isDeleted: z.boolean().default(false).optional(),
+});
