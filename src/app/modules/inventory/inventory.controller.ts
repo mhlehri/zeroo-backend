72417651043ea05199aaa8c17a88d302f420dbc0
mhlehri <lehrimirza101@gmail.com>
2025-03-03 17:07:46 +0600

    added get tag controller

--- src/app/modules/inventory/inventory.controller.ts
+++ src/app/modules/inventory/inventory.controller.ts
@@ -4,7 +4,7 @@ import sendResponse from "../../utils/sendResponse";
 
 import { RequestHandler } from "express";
 import AppError from "../../errors/AppError";
-import { addSizeIntoDB, addTagIntoDB } from "./inventory.service";
+import { addSizeIntoDB, addTagIntoDB, getTagsFromDB } from "./inventory.service";
 
 export const addSize = catchAsync(async (req, res) => {
   const { size } = req.body;
@@ -32,7 +32,13 @@ export const addTag = catchAsync(async (req, res) => {
   });
 });
 
-export const getTag = catchAsync(async (req, res) => {});
+export const getTag = catchAsync(async (req, res) => {
+  const result = await getTagsFromDB();
+  sendResponse(res, {
+    message: "Tags fetched successfully",
+    data: result,
+  });
+});
 export const getSize = catchAsync(async (req, res) => {});
 
 export const deleteTag: RequestHandler = catchAsync(async (req, res) => {});
